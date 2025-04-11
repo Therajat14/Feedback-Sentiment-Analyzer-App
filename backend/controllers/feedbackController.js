f8f4523b4ae96fb798e5cd18e44b823da5a68409
therajat14 <heylegend14@gmail.com>
2025-04-12 01:19:43 +0530

    feat: create controller for user feedback history and improve feedback submission

--- backend/controllers/feedbackController.js
+++ backend/controllers/feedbackController.js
@@ -33,3 +33,18 @@ export const submitFeedback = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+export const getUserFeedbacks = async (req, res) => {
+  try {
+    const userId = req.userId;
+    console.log(userId); // prints 67f9103e20311c9b98ada29e
+    const feedbacks = await Feedback.find({ userId }).sort({
+      createdAt: -1,
+    });
+
+    res.status(200).json(feedbacks);
+  } catch (error) {
+    console.error("Error fetching feedbacks:", error);
+    res.status(500).json({ message: "Server Error while fetching feedbacks" });
+  }
+};
