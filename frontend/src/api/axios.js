b6b5a4fd6d6077b8b79f84ec46909a00e6a57b1a
therajat14 <heylegend14@gmail.com>
2025-04-12 01:19:44 +0530

    feat: setup axios instance with token in Authorization header

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL: import.meta.env.VITE_API_URL, // 🔥 Adjust as per your backend
-  withCredentials: true, // if you are using cookies/auth
+  //   withCredentials: true, // if you are using cookies/auth
 });
 
 export default instance;
